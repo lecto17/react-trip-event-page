bc9239058557fdd8045e72b98b2ce0f31c6ae148
lecto17 <khwoo_rpsit@naver.com>
2021-10-03 13:56:41 +0900

    chore: API 환경변수 설정

---
+++ react-trip-event-page/src/config.js
@@ -0,0 +1,2 @@
+export const LOCATION_API = "https://d2828c62-eb78-4052-8143-257d053b1240.mock.pstmn.io/trips/";
+export const CATEGORY_API = "https://d2828c62-eb78-4052-8143-257d053b1240.mock.pstmn.io/categories/";
