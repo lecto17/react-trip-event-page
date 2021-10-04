5a7e2903e0579ca957d1e1b53a8b5db3fe8529a0
lecto17 <khwoo_rpsit@naver.com>
2021-10-04 15:25:18 +0900

    style: 지역별 안내 화면 기본 style 적용

--- react-trip-event-page/src/components/LocationInfoContent.js
+++ react-trip-event-page/src/components/LocationInfoContent.js
@@ -1,13 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function LocationInfoContent(props){
+function LocationInfoContent({content}){
   return (
     <>
-      <div>
-        {/* { console.log("props: ", props)} */}
-      </div>
+      <LocContentContainer>
+        <ImgWrapper>
+          <ContentImg src={content.img_url}/>
+        </ImgWrapper>
+        <ContentWrapper>
+          <div>
+            <ContentId>{ content.id < 10 ? "0"+content.id : content.id}.</ContentId>
+            <h2 style={{display: "inline"}}>{content.title}</h2>
+          </div>
+          <div>
+            <ContentData>{content.desc}</ContentData>
+            <Sight>추천명소: {content.sights}</Sight>
+          </div>  
+        </ContentWrapper>
+      </LocContentContainer>
     </>
   );
 };
+
+const LocContentContainer = styled.div`
+  margin: 20px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const ImgWrapper = styled.div`
+  display: inline;
+  margin: 0px 20px;
+`;
+
+const ContentImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border: 2px solid #B8D563;
+  border-radius: 50%;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  vertical-align: middle;
+`;
+
+const ContentId = styled.h2`
+  color: orange;
+  display: inline;
+`;
+
+
+const ContentData = styled.h6`
+  margin: 0;
+  display: inline-block; 
+  width: 300px; 
+  
+  white-space: normal; 
+  line-height: 1.2;
+  height: 3.6em;
+`;
+
+const Sight = styled.h6`
+  margin: 0;
+  color: #9BB65D;
+`;
 
 export default LocationInfoContent;
