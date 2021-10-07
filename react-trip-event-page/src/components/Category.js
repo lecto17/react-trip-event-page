5f2095a7885f9fd7df24128b3015081212cddbf5
lecto17 <khwoo_rpsit@naver.com>
2021-10-07 09:19:15 +0900

    feat: 카테코리 클릭 시 데이터 전환 작업

--- react-trip-event-page/src/components/Category.js
+++ react-trip-event-page/src/components/Category.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Category({category}){
+function Category({ category, cateState, setCateState}){
   return (
     <>
-      
       <CateBtnWrapper>
         {
           category.map(el => {
             return (
-              <CategoryButton key={el.id}>
+              <CategoryButton 
+                key={el.id} 
+                active={el.id===cateState} 
+                onClick={updateState(el.id, setCateState)}
+              >
                 {el.tag}
               </CategoryButton>
             )
           })
         }
-      </CateBtnWrapper>
-      
+      </CateBtnWrapper>  
     </>
   );
 };
 
+const updateState = (id, setCateState) => (event) => {
+  setCateState(id);
+}
+
 const CateBtnWrapper = styled.div`
     display: flex;
     justify-content: center;
+    padding: 0 30px;
 `;
 
 const CategoryButton = styled.button`
     border: 2px solid orange;
-    background-color: white;
-    color: orange;
+    background-color: ${props => props.active ? 'orange' : 'white'};
+    color: ${props => props.active ? 'white' : 'orange'};
     width: 150px;
     height: 50px;
+    font-weight: bold;
+    &:hover {
+      cursor: pointer;
+    }
 `;
 
 export default Category;
