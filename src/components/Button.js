3a527b1758c74f52c83aecf7ed91d67e7cd34467
lecto17 <khwoo_rpsit@naver.com>
2021-10-12 19:48:50 +0900

    comment: 불필요한 API호출 제거

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,18 +3,11 @@ import axios from 'axios';
 import '../css/Button.css';
 
 const Button = () => {
-    const clickBtn1 = async () => {
-        console.log("CLicked");
-        const response = await axios.get(
-          'https://d2828c62-eb78-4052-8143-257d053b1240.mock.pstmn.io/trips/'
-        )
-        console.log("res: ", response);
-    }
     return (
         <>
             <h1>Button Component</h1>
             <div className="btn-wrapper">
-                <button className="btn btn1" onClick={clickBtn1}>button 1</button>
+                <button className="btn btn1">button 1</button>
                 <button className="btn btn2">button 2</button>
                 <button className="btn btn3">button 3</button>
             </div>
